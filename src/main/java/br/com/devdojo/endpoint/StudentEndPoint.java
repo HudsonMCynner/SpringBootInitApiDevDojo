fef7126ab0dce410b9fb3370ae2e76d54d00de17
Hudson <hudsonmachado2012@gmail.com>
2020-09-26 13:45:43 -0400

    Spring Boot Essentials 08 - Adicionando Spring Data JPA com PostgreSQL pt 01

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -2,59 +2,53 @@ package br.com.devdojo.endpoint;
 
 import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
-import br.com.devdojo.util.DateUtil;
+import br.com.devdojo.repositiry.StudentRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-import static java.util.Arrays.asList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("students")
 public class StudentEndPoint {
 
-    private final DateUtil dateUtil;
+    private final StudentRespository studentDAO;
 
     @Autowired
-    public StudentEndPoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
+    public StudentEndPoint(StudentRespository studentDAO) {
+        this.studentDAO = studentDAO;
     }
 
     @GetMapping
     public ResponseEntity<?> listAll() {
-        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+        return new ResponseEntity<>(studentDAO.findAll(), HttpStatus.OK);
     }
 
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable int id) {
-        Student student = new Student();
-        student.setId(id);
-        int index = Student.studentList.indexOf(student);
-        if (index == -1)
+    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+        Optional<Student> student = studentDAO.findById(id);
+        if (student == null)
             return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student) {
-        Student.studentList.add(student);
-        return new ResponseEntity<>(student, HttpStatus.OK);
+        return new ResponseEntity<>(studentDAO.save(student), HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody Student student) {
-        Student.studentList.remove(student);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        studentDAO.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
-        Student.studentList.remove(student);
-        Student.studentList.add(student);
+        studentDAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
---
+++ src/main/java/br/com/devdojo/repositiry/StudentRespository.java
@@ -0,0 +1,10 @@
+package br.com.devdojo.repositiry;
+
+import br.com.devdojo.model.Student;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface StudentRespository extends CrudRepository<Student, Long> {
+    List<Student> findByName(String name);
+}
