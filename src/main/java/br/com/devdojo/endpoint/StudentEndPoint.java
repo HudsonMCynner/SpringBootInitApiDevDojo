6d85cd0205f56102844774d20efc494d8359ef23
Hudson <hudsonmachado2012@gmail.com>
2020-09-26 12:47:10 -0400

    Spring Boot Essentials 07 - Padrões REST e PUT e DELETE pt 03

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -23,13 +23,13 @@ public class StudentEndPoint {
         this.dateUtil = dateUtil;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<?> listAll() {
         return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable int id) {
         Student student = new Student();
         student.setId(id);
@@ -39,9 +39,22 @@ public class StudentEndPoint {
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student) {
         Student.studentList.add(student);
         return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
