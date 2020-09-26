f1489dfc8eec76d7ccd844da3878507475bce4d1
Hudson <hudsonmachado2012@gmail.com>
2020-09-26 12:38:30 -0400

    Spring Boot Essentials 06 - Padrões REST e POST pt 02

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -6,10 +6,7 @@ import br.com.devdojo.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,9 +33,15 @@ public class StudentEndPoint {
     public ResponseEntity<?> getStudentById(@PathVariable int id) {
         Student student = new Student();
         student.setId(id);
-       int index = Student.studentList.indexOf(student);
-       if (index == -1)
-           return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
-       return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+        int index = Student.studentList.indexOf(student);
+        if (index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 }
