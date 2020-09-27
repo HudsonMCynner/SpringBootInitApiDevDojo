6f4d46ca3882a92d22a41f16bb2e55899e5e017e
Hudson <hudsonmachado2012@gmail.com>
2020-09-27 09:30:22 -0400

    Spring Boot Essentials 12 - Tratamento de erros em REST pt 03 - Transações

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @RestController
@@ -35,7 +36,13 @@ public class StudentEndPoint {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<?> save(@RequestBody Student student) {
+        studentDAO.save(student);
+        studentDAO.save(student);
+        if (true) {
+            throw new RuntimeException("Teste Transaction");
+        }
         return new ResponseEntity<>(studentDAO.save(student), HttpStatus.OK);
     }
 
