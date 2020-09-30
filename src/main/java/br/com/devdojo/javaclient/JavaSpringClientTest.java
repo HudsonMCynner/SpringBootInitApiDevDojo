ec66e0dd8cf07b926de8db76063649bf725ab2b4
Hudson <hudsonmachado2012@gmail.com>
2020-09-29 20:11:06 -0400

    Spring Boot Essentials 22 - Cliente Spring com RestTemplate pt 02 GET Pageable

--- src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/javaclient/JavaSpringClientTest.java
@@ -1,5 +1,6 @@
 package br.com.devdojo.javaclient;
 
+import br.com.devdojo.model.PageableResponse;
 import br.com.devdojo.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
@@ -7,24 +8,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class JavaSpringClientTest {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplateBuilder().rootUri("http://localhost:8080/v1/protected/students")
-                .basicAuthentication("hud", "banana").build();
+                .basicAuthorization("hud", "banana").build();
 
-        Student student = restTemplate.getForObject("/{id}", Student.class, 8);
-        System.out.println(student);
+//        Student student = restTemplate.getForObject("/{id}", Student.class, 8);
+//        System.out.println(student);
+//
+//        ResponseEntity<Student> forEntityStudent = restTemplate.getForEntity("/{id}", Student.class, 8);
+//        System.out.println(forEntityStudent.getBody());
+//
+//        Student[] students = restTemplate.getForObject("/", Student[].class);
+//        System.out.println(Arrays.toString(students));
+//
+//        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
+//        System.out.println(exchange.getBody());
 
-        ResponseEntity<Student> forEntityStudent = restTemplate.getForEntity("/{id}", Student.class, 8);
-        System.out.println(forEntityStudent.getBody());
-
-        Student[] students = restTemplate.getForObject("/", Student[].class);
-        System.out.println(Arrays.toString(students));
-
-        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
-        System.out.println(exchange.getBody());
+        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Student>>() {
+                });
+        System.out.println(exchange);
     }
 }
